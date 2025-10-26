20cd68079f7750e8bb607ce2cff3a6c82018f127
Dilan <dilanartiles@gmail.com>
2025-10-25 21:42:19 -0600

    Base del Proyecto:
    - GUI base, faltan logos y cambios
    - Clase Producto padre
    - Clase Auxiliar con metodos de excepciones
      para String, Int y Double
    - Boton Limpiar

--- Proyecto_ClienteServidor/src/Main.java
+++ Proyecto_ClienteServidor/src/Main.java
@@ -12,7 +12,15 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
+        
+        // Muestra interfaz principal
+        MenuInicio menuInicio = new MenuInicio();
+        menuInicio.setVisible(true);
+        menuInicio.setLocationRelativeTo(null);
+        
         
     }
     
